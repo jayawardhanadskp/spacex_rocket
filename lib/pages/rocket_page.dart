aac4ed6c0dc5b38cddcbcc4bf1eca257e63e0a14
kasun <kasunpradeep.ds@gmail.com>
2024-06-27 21:49:07 +0530

    rocket page

---
+++ lib/pages/rocket_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:spacex_rocket/data/rocket.dart';
+import 'package:spacex_rocket/services/rocket_service.dart';
+
+class RocketPage extends StatefulWidget {
+  const RocketPage({super.key});
+
+  @override
+  State<RocketPage> createState() => _RocketPageState();
+}
+
+class _RocketPageState extends State<RocketPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Rocket'),
+          centerTitle: true,
+        ),
+        body: FutureBuilder<Rocket?>(
+          future: RocketService.getRocket(),
+          builder: (BuildContext context, AsyncSnapshot<Rocket?> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator.adaptive());
+            } else if (snapshot.hasError) {
+              return Center(child: Text(snapshot.error.toString()));
+            }
+            Rocket? rocket = snapshot.data;
+
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  width: double.infinity,
+                ),
+                Text(
+                  rocket?.name ?? '',
+                  style: const TextStyle(fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  rocket?.description ?? '',
+                  style: const TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  rocket?.firstFlight ?? '',
+                  style: const TextStyle(fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Image.network(rocket?.image ?? '')
+              ],
+            );
+          },
+        ));
+  }
+}
