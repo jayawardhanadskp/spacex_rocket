b489cef3a45f2d16d88d109053fffdf4ec21d980
kasun <kasunpradeep.ds@gmail.com>
2024-07-01 10:42:20 +0530

    rocket service

--- lib/services/rocket_service.dart
+++ lib/services/rocket_service.dart
@@ -6,12 +6,27 @@ import 'package:spacex_rocket/config.dart';
 import '../data/rocket.dart';
 
 class RocketService {
+  // get one rocekt
   static Future<Rocket?> getRocket() async {
     final response = await http.get(
         Uri.parse("${Config.BACKEND_URL}rockets/5e9d0d96eda699382d09d1ee"));
 
     if (response.statusCode == 200) {
       return Rocket.fromJSON(jsonDecode(response.body));
+    }
+    return null;
+  }
+
+  // get rocket list
+  static Future<List<Rocket>?> getRockets() async {
+    final response = await http.get(Uri.parse("${Config.BACKEND_URL}rockets"));
+
+    if (response.statusCode == 200) {
+      // get all list and map
+      return jsonDecode(response.body).map<Rocket>((data) {
+        // return 1 rocket
+        return Rocket.fromJSON(data);
+      }).toList();
     }
     return null;
   }
